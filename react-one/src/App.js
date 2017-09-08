0ed3fb55e0c8c27a8b76256936d0a97d730bed6e
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-09-07 22:04:08 -0700

    Added sass to the project

--- react-one/src/App.js
+++ react-one/src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './sass/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Here we are</h1>
       </div>
     );
   }

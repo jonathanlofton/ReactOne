a37996b4eee727b128d771fcd1fb620564816f01
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-09-09 14:26:58 -0700

    single page search app is functional

--- react-one/src/App.js
+++ react-one/src/App.js
@@ -7,10 +7,11 @@ class App extends Component {
     super(props)
     this.state = {
       searchQuery: '',
-      characters: [],
+      characters: null,
     }
     this.fetchResults = this.fetchResults.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
+    this.test = this.test.bind(this);
   }
 
   fetchResults(e) {
@@ -19,40 +20,53 @@ class App extends Component {
     })
     fetch(`https://swapi.co/api/people/?search=${e.target.value}`).then(function(response) {
      return response.json();
-   }).then((response) => {this.handleSearch(response)});
+   }).then((response) => {this.handleSearch(response.results)});
   }
 
-  handleSearch(starWars) {
-    if (!starWars) {
-      return (
-        <p>loading</p>
-      )
-    }
-      this.setState({
-        characters: starWars.results
-      })
-      console.log(this.state.characters)
-      let characters = this.state.characters
-      return (
-        <ul>
-          {characters.map(character => <li>{character.name}</li>)}
-        </ul>
-      )
+  handleSearch(char) {
+    this.setState({
+      characters: char
+    })
+    console.log(this.state.characters)
+    this.test;
+  }
+
+  test() {
+    let characters = this.state.characters
+    return (
+      <ul>
+        {characters.map(character => <li>{character.name}</li>)}
+      </ul>
+    )
   }
 
 
   render() {
+    if (this.state.characters === null) {
+      return (
+        <div className="App">
+          <input
+            ref={(input) => { this.searchInput = input; }}
+            type={'text'}
+            onChange={this.fetchResults}
+            value={this.state.searchQuery}
+            placeholder={'search'}
+            >
+          </input>
+        </div>
+      )
+    }
     return (
       <div className="App">
         <input
           ref={(input) => { this.searchInput = input; }}
           type={'text'}
-          onChange={(e) => {this.fetchResults(e)}}
+          onChange={this.fetchResults}
           value={this.state.searchQuery}
           placeholder={'search'}
           >
         </input>
-        {this.handleSearch()}
+        {this.test()}
       </div>
     );
   }

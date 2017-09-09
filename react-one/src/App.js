b1682de4f6aa97928c0ad09f8f3e7f351a654741
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-09-08 23:35:14 -0700

    search component created

--- react-one/src/App.js
+++ react-one/src/App.js
@@ -3,10 +3,56 @@ import logo from './logo.svg';
 import './sass/App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchQuery: '',
+      characters: [],
+    }
+    this.fetchResults = this.fetchResults.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  fetchResults(e) {
+    this.setState({
+      searchQuery: e.target.value
+    })
+    fetch(`https://swapi.co/api/people/?search=${e.target.value}`).then(function(response) {
+     return response.json();
+   }).then((response) => {this.handleSearch(response)});
+  }
+
+  handleSearch(starWars) {
+    if (!starWars) {
+      return (
+        <p>loading</p>
+      )
+    }
+      this.setState({
+        characters: starWars.results
+      })
+      console.log(this.state.characters)
+      let characters = this.state.characters
+      return (
+        <ul>
+          {characters.map(character => <li>{character.name}</li>)}
+        </ul>
+      )
+  }
+
+
   render() {
     return (
       <div className="App">
-        <h1>Here we are</h1>
+        <input
+          ref={(input) => { this.searchInput = input; }}
+          type={'text'}
+          onChange={(e) => {this.fetchResults(e)}}
+          value={this.state.searchQuery}
+          placeholder={'search'}
+          >
+        </input>
+        {this.handleSearch()}
       </div>
     );
   }

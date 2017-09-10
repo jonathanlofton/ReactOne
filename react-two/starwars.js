f9edc45c7dd8a14232bd929b4969a5d55715c903
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-09-09 20:26:01 -0700

    refactored vanilla javascript star wars search bar

--- react-two/starwars.js
+++ react-two/starwars.js
@@ -1,16 +1,19 @@
 
 let search = ""
 
-const fetchCharacters = (go) => {
-  search += go.data
-  console.log(go)
+const fetchCharacters = (searchResult) => {
+  console.log(searchResult.data)
+  if (searchResult.data === null) {
+    search = search.slice(0, search.length - 1)
+  } else {
+    search += searchResult.data
+  }
   fetch(`https://swapi.co/api/people/?search=${search}`)
   .then( response => { return response.json() } )
   .then( characters => renderList(characters))
 }
 
 const renderList = (characters) => {
-  console.log(characters.value)
   const html = characters.results.map(character => {
     return `
       <li>
@@ -26,4 +29,4 @@ let searchBar = document.querySelector('.search')
 let characterList = document.querySelector('.characterList')
 
 
-searchBar.addEventListener('input', (go) => fetchCharacters(go))
+searchBar.addEventListener('input', (searchResult) => fetchCharacters(searchResult))

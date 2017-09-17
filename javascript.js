6587c9b29b5298fb25aec670cb9957615b5f8f62
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-09-16 22:52:56 -0700

    refactored

--- javascript.js
+++ javascript.js
@@ -1,23 +1,20 @@
 
-function Dog() {
-        this.name = ""
+function Dog(name) {
+        this.name = name
         this.age = 0
 }
 
-function Person() {
-    this.name = ""
+function Person(name) {
+    this.name = name
     this.age = 0
 }
-
 
 var birthday = function(num) {
     this.age += num
 } 
 
-let spunk = new Dog();
-spunk.name = "spunky"
-let john = new Person();
-john.name = "John"
+let spunk = new Dog("spunky");
+let john = new Person("john");
 
 // call or apply when you can use the same function for different classes / objects 
 // use call apply or bind when you want to pass a different this value to the function. 
@@ -29,10 +26,6 @@ birthday.call(spunk, 7)
 console.log(spunk.age)
 birthday.apply(spunk, [7])
 console.log(spunk.age)
-
-// birthday.bind(john)(7)
-// console.log(john.age)
-
 
 // use bind when you want to have the context of an object
 // assigned in a variable

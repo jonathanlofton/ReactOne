b0d60f3b752f15ddcfa2889b1b06535f191f31a0
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-09-16 17:38:36 -0700

    added some notes to my javascript file

--- javascript.js
+++ javascript.js
@@ -20,6 +20,8 @@ let john = new Person();
 john.name = "John"
 
 // call or apply when you can use the same function for different classes / objects 
+// use call apply or bind when you want to pass a different this value to the function. 
+
 
 birthday.call(john, 9)
 console.log(john.age)
@@ -34,12 +36,24 @@ console.log(spunk.age)
 
 // use bind when you want to have the context of an object
 // assigned in a variable
+
+// Bind explicitly states the object that is bound to this
 var check = birthday.bind(john)
 check(7);
 console.log(john)
 check(7);
 console.log(john)
 
-var check2 = () => birthday.call(john, 7)
-check2();
+var check2 = (x) => birthday.apply(john, x)
+check2([3, 4, 5]);
 console.log(john)
+check2(4);
+console.log(john)
+
+
+
+
+// 'this' actually makes your javascript code more ambiguous because reffering to 
+// the variable name could actually be mistaken for another global variable of the same name.
+
+// variadic functions are functions that use multiple arguments instead of a set number of arguments

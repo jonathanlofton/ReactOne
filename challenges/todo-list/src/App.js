d067e7c4652f07ab5b91f533f5308738c011e722
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-09-23 13:02:59 -0700

    todo list react app completed

--- challenges/todo-list/src/App.js
+++ challenges/todo-list/src/App.js
@@ -1,8 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Data from './data';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: [],
+      title: '',
+      body: ''
+    }
+    this.createTodo = this.createTodo.bind(this)
+    this.completedTodo = this.completedTodo.bind(this)
+  }
+
+  componentWillMount() {
+    this.setState({
+      todos: Data
+    })
+  }
+
+  completedTodo(todo) {
+    console.log(todo)
+    const completed = todo.finished ? false : true 
+    todo.finished = completed
+    const newList = Object.assign({}, this.state.todos, { [todo.id]: todo})
+    this.setState({ todos: newList })
+  }
+
+  createTodo(e) {
+    e.preventDefault();
+
+    let newTodo = {
+      title: this.state.title,
+      body: this.state.body,
+      finished: false
+    }
+    
+    let newState = this.state.todos
+    newState.push(newTodo)
+    
+    this.setState({
+      todos: newState,
+      title: '',
+      body: ''
+    })
+  }
+  
   render() {
     return (
       <div className="App">
@@ -10,9 +55,27 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul style={{listStyleType: 'none'}}>
+          {Data.map((todo, idx) => (
+            <li key={idx}>
+              <p style={{ fontSize: 20 }}>
+                {todo.title}
+              </p>
+              <p>{todo.body}</p>
+              <button 
+                style={{ background: todo.finished ? 'green' : 'red'}} 
+                onClick={() => this.completedTodo(todo)}
+                >
+                {todo.finished ? 'completed' : 'incomplete'}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <form onSubmit={this.createTodo} style={{ display: 'flex', flexDirection: 'column', margin: 'auto', width: 300 }}>
+          Title: <input type='text' value={this.state.title} onChange={ e => this.setState({ title: e.target.value })}/>
+          Body: <input type='text' value={this.state.body} onChange={ e => this.setState({ body: e.target.value })}/>
+          <button>Create Todo</button>
+        </form>
       </div>
     );
   }
---
+++ challenges/todo-list/src/data.js
@@ -0,0 +1,16 @@
+const Data = [
+  {
+    id: 0,
+    title: 'Haircut',
+    body: 'Go get a haircut dumbass',
+    finished: false
+  },
+  { 
+    id: 1,
+    title: 'Groceries',
+    body: 'Go get groceries please',
+    finished: false
+  }
+]
+
+export default Data

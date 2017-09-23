b04b5fc1b039751814ae4535c6c569dac41e75f7
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-09-23 11:30:24 -0700

    added star wars search to react app

--- challenges/drop-down-site/src/App.js
+++ challenges/drop-down-site/src/App.js
@@ -7,33 +7,51 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      forceChange: true
+      starwars: '',
+      characters: []
     }
     this.handleClick = this.handleClick.bind(this);
+    this.searchStarwars = this.searchStarwars.bind(this);
+    this.fetchStarWars = this.fetchStarWars.bind(this);
+  }
+
+  fetchStarWars() {
+    if (this.state.starwars !== null) {
+      fetch(`https://swapi.co/api/people/?search=${this.state.starwars}`)
+        .then(response => { return response.json() })
+        .then(characters => this.setState({
+          characters: characters.results
+        }))
+    } else {
+      this.setState({ characters: []})
+    }
+  }
+
+  searchStarwars(e) {
+    this.setState({ starwars: e.target.value })
+    console.log(e.target.value)
+    this.fetchStarWars()
   }
 
   handleClick(header) {
     header.hidden.true = header.hidden.true ? false : true 
-    this.setState({
-      forceChange: this.state.forceChange ? false : true 
-    })
-    console.log(header)
+    // will force the component to change
+    this.setState({})
   }
   
   render() {
-    console.log(Data)
     let hiddenContent;
     const doodoo = Data.map((header, idx) => (
-      <li>
-        <li key={idx} onClick={() => this.handleClick(header)}>
+      <li key={idx}>
+        <p onClick={() => this.handleClick(header)}>
           {header.header}
-        </li>
+        </p>
         <p>
           {hiddenContent = header.hidden.true ? null : header.hidden.content}
         </p>
       </li>
     ))
-
+    const starwars = this.state.characters
     return (
       <div className="App">
         <div className="App-header">
@@ -42,6 +60,14 @@ class App extends Component {
         </div>
         <ul>
           {doodoo}
+        </ul>
+        <input type='text' onChange={this.searchStarwars}/>
+        <ul>
+          {starwars.map((character, idx) => (
+            <li key={idx}>
+              {character.name}
+            </li>
+          ))}
         </ul>
       </div>
     );

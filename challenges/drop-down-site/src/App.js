e8f460194f09de08c7c58585ea8fef6e6181db55
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-09-23 11:07:17 -0700

    react drop down created

--- challenges/drop-down-site/src/App.js
+++ challenges/drop-down-site/src/App.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Data from './data';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      forceChange: true
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(header) {
+    header.hidden.true = header.hidden.true ? false : true 
+    this.setState({
+      forceChange: this.state.forceChange ? false : true 
+    })
+    console.log(header)
+  }
+  
   render() {
+    console.log(Data)
+    let hiddenContent;
+    const doodoo = Data.map((header, idx) => (
+      <li>
+        <li key={idx} onClick={() => this.handleClick(header)}>
+          {header.header}
+        </li>
+        <p>
+          {hiddenContent = header.hidden.true ? null : header.hidden.content}
+        </p>
+      </li>
+    ))
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {doodoo}
+        </ul>
       </div>
     );
   }
 }
 
 export default App;
+// {
+//   Data.map((header, idx) => (
+//     <li key={idx} onClick={() => this.handleClick(header)}>
+//       {header.id}
+//       {header.header}
+//     </li>
+//   ))
+// }
---
+++ challenges/drop-down-site/src/data.js
@@ -0,0 +1,18 @@
+const Data = 
+   [{
+      id: 1,
+      header: "header 1",
+      hidden: {
+        true: true,
+        content: "hidden 1",
+      },
+    },
+    {
+      id: 2,
+      header: "header 2",
+      hidden: {
+        true: true,
+        content: "hidden 2",}
+    }]
+
+export default Data;

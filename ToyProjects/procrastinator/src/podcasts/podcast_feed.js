6bac5e3df3d130627b4e9dc8e5568d5624d84d37
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-10-04 15:24:12 -0700

    Fetching beers from the beersapi

--- ToyProjects/procrastinator/src/podcasts/podcast_feed.js
+++ ToyProjects/procrastinator/src/podcasts/podcast_feed.js
@@ -4,26 +4,59 @@ import config from '../config.js'
 class PodcastFeed extends React.Component {
   constructor(props) {
     super(props)
-    this.fetchBeers = this.fetchBeers.bind(this);
+    this.state ={
+      beerName: '',
+      beerInfo: {},
+    }
   }
 
   componentDidMount() {
-    this.fetchBeers('budweiser');
   }
 
-  fetchBeers(beerName) {
+  fetchBeerByName() {
+    console.log(this.state.beerName)
     let { beerKey } = config;
     let proxy = `https://cors-anywhere.herokuapp.com/`
-    let url = `http://api.brewerydb.com/v2/beers?key=${beerKey}&name=${beerName}`
+    let url = `http://api.brewerydb.com/v2/beers?key=${beerKey}&name=${this.state.beerName}`
     fetch(proxy + url)
     .then(res => res.json())
-    .then(res => console.log(res))
+    .then(res => this.setState({ beerInfo: res }))
+  }
+
+  handleChange(e) {
+    this.setState({
+      beerName: e.target.value
+    })
   }
 
   render() {
+    const { beerInfo } = this.state;
+    let beer;
+    console.log(beerInfo)
+    if (beerInfo.data) {
+      const { data } = beerInfo;
+      beer = data.map((beer, idx) => (
+        <li key={idx}>
+          <img src={`${beer.labels.icon}`} alt={`${beer.name}`}/>
+          <p>{beer.name}</p>
+          <p>{beer.style.description}</p>
+        </li>
+      )) 
+    } else {
+      beer = null
+    }
+    
+    
     return(
      <div>
-        <h1 className='body-header'>Podcast Feed</h1>
+        <h1 className='body-header'>Beer Feed</h1>
+        <form onSubmit={() => this.fetchBeerByName()}>
+          <input type='text' value={this.state.beerName} onChange={(e) => this.handleChange(e)}/>
+          <button>Search</button>
+        </form>
+        <ul>
+          { beer }
+        </ul>
      </div> 
     )
   }

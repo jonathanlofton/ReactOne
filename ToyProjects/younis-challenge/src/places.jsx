65aa436119f3b3aa46902f33f96bcd179a70bc9a
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-10-08 10:08:34 -0700

    Places component created and added to App

---
+++ ToyProjects/younis-challenge/src/places.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Places extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>PLACES</h1>
+      </div>
+    )
+  }
+}
+
+export default Places;

999e2da34ca9c5c3f06954aee73535f05174ea22
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-10-30 21:55:37 -0700

    Added a header component

--- ToyProjects/rails_todo_app/frontend/components/LandingPage/scaffold.jsx
+++ ToyProjects/rails_todo_app/frontend/components/LandingPage/scaffold.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import LandingPageContainer from './landing_page_container';
 import LoginContainer from '../Login/login_container';
+import Header from '../Header/header';
 import { Route } from 'react-router-dom';
 
 class Scaffold extends React.Component {
   render() {
     return (
       <div className="landing-page">
-        <div className="header">
-        </div>
+          <Route path="/" component={Header} />
         <div className="body">
           <Route exact path="/" component={LoginContainer} />
           <Route path="/home" component={LandingPageContainer} />

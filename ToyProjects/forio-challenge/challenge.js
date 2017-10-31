caf44096a1ddc59f89d117496bcf504716bf78c1
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-10-30 19:09:40 -0700

    Added a new Toyproject to the directory

---
+++ ToyProjects/forio-challenge/challenge.js
@@ -0,0 +1,31 @@
+// DOM Manipulation question on JSFiddle
+
+<ul>
+  <li>5</li>
+  <li>8</li>
+  <li>3</li>
+  <li>12</li>
+  <li>-2</li>
+</ul>
+
+// given the elements above, edit it to have the final form look like
+
+// have a maximum of 2 list tags in a ul and make sure the values are sorted
+<ul>
+  <li>-2</li>
+  <li>3</li>
+</ul>
+<ul>
+  <li>5</li>
+  <li>8</li>
+</ul>
+<ul>
+  <li>12</li>
+</ul>
+
+
+// 2nd problem
+// sort the values RGB inline
+
+const array = [ 'R', 'G', 'B', 'R', 'R', 'G', 'G', 'B'] 
+// => [R,R,G,G,G,B,B]

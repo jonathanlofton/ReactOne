178e99f3e893f1ad0ea630e8f2b5c135ba2a8ee5
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-11-14 11:42:11 -0800

    Node server is now serving an index page

--- ToyProjects/node-mongo-express-crud/server.js
+++ ToyProjects/node-mongo-express-crud/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log('listening to 3000...')
 })
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+
+// app.get(path, callback)
+// path === / in this case because the browser is looking for
+// localhost:3000/ <== that last slash is what its looking for
+
+// callback === is a function that is executed when that path is 
+// being hit

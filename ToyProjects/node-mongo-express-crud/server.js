d9f5e6f71b71dc569752745aee2bde89fe393a4a
Jonathan M. Lofton <jonathanlofton91@gmail.com>
2017-11-14 12:41:38 -0800

    Mongodb database setup and running through mongo labs

--- ToyProjects/node-mongo-express-crud/server.js
+++ ToyProjects/node-mongo-express-crud/server.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(3000, () => {
-  console.log('listening to 3000...')
+let db;
+
+MongoClient.connect('mongodb://jlofton:12345@ds163705.mlab.com:63705/mongo-test-1', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening to 3000...')
+  })
 })
+
+
+
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
 app.post('/quotes', (req, res) => {
-  console.log("HELLLOO")
+  console.log(req.body)
 })
+
+
 // app.get(path, callback)
 // path === / in this case because the browser is looking for
 // localhost:3000/ <== that last slash is what its looking for
